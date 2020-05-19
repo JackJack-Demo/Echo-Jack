7bb594986f7a2fb63d86ec92849d81bb583cc661
ramcharan <sairamcharansudagoni@gmail.com>
2020-05-19 07:00:20 +0000

    Test

--- bot.js
+++ bot.js
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 const { ActivityHandler, MessageFactory } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
@@ -8,7 +5,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `EchoDumbu: ${ context.activity.text }`;
+            const replyText = `EchoJack: ${ context.activity.text }`;
             await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
